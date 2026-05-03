77a7cf749fae5bd39f5896443110e9c0e325cc29
AINADOU Mondoukpe <manakelainadou@gmail.com>
2026-05-03 14:57:45 +0200

    ajout du convertisseur avec dollar

---
+++ Convertisseur.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Convertisseur {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+
+        System.out.println("=== CONVERTISSEUR ===");
+
+        System.out.println("1. FCFA -> Euro");
+        System.out.println("2. Euro -> FCFA");
+
+        System.out.print("Choisis une option (1 ou 2) : ");
+        choix = scanner.nextInt();
+
+        if (choix == 1) {
+
+            System.out.print("Entre le montant en FCFA : ");
+            double fcfa;
+
+            while (true) {
+                fcfa = scanner.nextDouble();
+            
+                if (fcfa >= 0) {
+                    break;
+                } else {
+                    System.out.print("⚠️ Le montant doit être positif. Réessaie : ");
+                }
+            }
+
+            double euro = fcfa / 655.957;
+
+            System.out.println("Resultat : " + euro + " Euro");
+
+        } else if (choix == 2) {
+
+            System.out.print("Entre le montant en Euro : ");
+            double euro;
+
+            while (true) {
+                euro = scanner.nextDouble();
+            
+                if (euro >= 0) {
+                    break;
+                } else {
+                    System.out.print("⚠️ Le montant doit être positif. Réessaie : ");
+                }
+            }
+
+            double fcfa = euro * 655.957;
+
+            System.out.println("Resultat : " + fcfa + " FCFA");
+
+        } else {
+
+            System.out.println("Choix invalide !");
+        }
+
+        scanner.close();
+    }
+}
